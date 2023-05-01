ca9716f716abbbf3b9e98bb521616773d3b4e96b
Yusuf Büyükdağ <123562650+ybuyukdag@users.noreply.github.com>
2023-05-01 11:48:19 +0300

    Update mongodb-connection.py

--- mongodb-connection.py
+++ mongodb-connection.py
@@ -2,7 +2,7 @@ import pymongo
 
 
 #myclient = pymongo.MongoClient("mongodb://localhost:27017")
-myclient = pymongo.MongoClient("mongodb+srv://ybuyukdag:Ysfmdb103@cluster0.tyezpvw.mongodb.net/node-app?retryWrites=true&w=majority")
+myclient = pymongo.MongoClient("mongodb+srv://ybuyukdag:(*PASSWORD*)@cluster0.tyezpvw.mongodb.net/node-app?retryWrites=true&w=majority")
 
 
 mydb = myclient["node-app"]
